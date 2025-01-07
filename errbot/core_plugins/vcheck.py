54eb20ab5ec4eabba4b653742204e2a031ab255b
Alexander Kovrigin <alexander.kovrigin@jetbrains.com>
2025-01-07 18:43:04 +0100

    waleko__diff-9d9b3__id_180

--- errbot/core_plugins/vcheck.py
+++ errbot/core_plugins/vcheck.py
@@ -16,7 +16,6 @@ installed_version = version2tuple(VERSION)
 
 PY_VERSION = ".".join(str(e) for e in sys.version_info[:3])
 
-
 class VersionChecker(BotPlugin):
     connected = False
     activated = False
