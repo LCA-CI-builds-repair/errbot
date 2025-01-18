34fb7c94705d0204b0ffabb13391fea4fd160af0
ubuntu <ubuntu@ip-10-0-1-44>
2025-01-18 10:53:47 +0000

    waleko__claude-3.5-sonnet_ce59__id_181

--- tests/commands_test.py
+++ tests/commands_test.py
@@ -15,7 +15,8 @@ extra_plugin_dir = path.join(path.dirname(path.realpath(__file__)), "dummy_plugi
 
 
 def test_root_help(testbot):
-    assert "All commands" in testbot.exec_command("!help")
+    assert "A new plugin repository has been installed correctly from" in testbot.pop_message(
+        timeout=60)All commands" in testbot.exec_command("!help")
 
 
 def test_help(testbot):
